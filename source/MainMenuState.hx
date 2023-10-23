703e03b43d2b628c8999c6ecee5c656dd63acd9c
kangel <132186966+kangeluwu@users.noreply.github.com>
2023-10-24 07:03:56 +0800

    Updated MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -259,7 +259,6 @@ function makeHaxeState(usehaxe:String, path:String, filename:String) {
 	interp.variables.set("currentMainMenuState", this);
 	interp.variables.set("add", add);
 	interp.variables.set("remove", remove);
-	interp.variables.set("fromRGB", fromRGB);
 	interp.variables.set("X", FlxAxes.X);
 	interp.variables.set("Y", FlxAxes.Y);
 	interp.variables.set("Application", Application);
